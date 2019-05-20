4e48d7e33d64d1ce75ba3dc1a30b9dce43ad3c9f
Steffen Kaminski <steffen.kaminski@kuleuven.be>
2019-05-20 09:37:38 +0200

    upload use_case for belgium data 2017
    @u0128861 finally. Tho the data needs to be manually checked for each "irregularities" which is printed. You cannot collect all errors in the measurement data through code...

---
+++ custom/results/run_example.jl
@@ -0,0 +1,19 @@
+# using Revise
+
+using RepresentativeDaysFinders
+using JuMP
+# using GLPK
+# using Gurobi
+##################################################################################
+# Specify location of config-file
+##################################################################################
+config_file = normpath(joinpath(@__DIR__, "scenarios", "test_study.yaml"))
+
+
+##
+using Cbc
+dft = findRepresentativeDays(config_file, with_optimizer(Cbc.Optimizer; seconds = 60))
+# findRepresentativeDays(config_file, with_optimizer(GLPK.Optimizer; presolve=true, msg_lev=GLPK.MSG_ALL, tm_lim=180*1000))
+# Juno.@enter findRepresentativeDays(config_file, with_optimizer(GLPK.Optimizer; presolve=true, msg_lev=GLPK.MSG_ALL, tm_lim=180*1000))
+
+RepresentativeDaysFinders.create_plots(dft)
