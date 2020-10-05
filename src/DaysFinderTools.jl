b45641ead348bf8d002f064b3a6e7c1ae51e7842
Sebastian Gonzato <sebastian_gonzato@protonmail.com>
2020-10-05 14:58:54 +0200

    Can have mandatory periods now.

--- src/DaysFinderTools.jl
+++ src/DaysFinderTools.jl
@@ -765,11 +765,6 @@ function timePeriodClustering(dft::DaysFinderTool)
     # Notation is based on the following paper:
     # [1] S. Pineda and J. M. Morales, “Chronological time-period clustering for optimal capacity expansion planning with storage,” IEEE Trans. Power Syst., vol. 33, no. 6, pp. 7162–7170, 2018.
 
-    # DEbugging stuff, ignore or delete
-    # config_file = "/home/u0128861/Desktop/RepDays4Storage/rep-days-4-storage/input_data/Elia_2017.yaml"
-    # dft = DaysFinderTool(config_file)
-    # populateDaysFinderTool!(dft)
-
     type = get(dft.config["solver"], "Method", "hierarchical clustering")
     N = dft.N_total_periods # Initial number of clusters / periods
     NC = dft.N_total_periods # Current number of clusters
@@ -786,11 +781,22 @@ function timePeriodClustering(dft::DaysFinderTool)
     ] # Cluster features vector
     xbar = copy(x) # at least initially
 
+    # TODO: Possible performance boosts:
+    # 1. Changing the order of the for loops to be j then i ->
+    # be careful with this though.
+    # 2. Using @inbounds -> avoids check in the array bounds when running
+    # 3. Using Threads.@threads (might not speed things up though), generally bad idea
+    # 5. Can also try out @simd apparently?
+    # 6. Get rid of if statements in loops - just make sure i != j
+
     # Define dissimilarity matrix
-    # Since we only care about the (unordered) pairs, we only define the upper triangle of the matrixs
+    # Since we only care about the (unordered) pairs,
+    # we only define the upper / lower triangle of the matrix
+    # For hierarchical clustering, D is lower triangular and we compute 1/D
+    # For CTPC, D is upper triangular with only diagonals defined
     D = fill(Inf32, NC, NC)
     # NOTE: Can also define D as UpperTriangular
-    # but this leads to 0 entries which is annoying when
+    # but this leads to 0 entries which can be annoying when
     # trying to find the minimum
     Threads.@threads for j in 1:NC
         if type == "chronological time period clustering"
@@ -814,29 +820,33 @@ function timePeriodClustering(dft::DaysFinderTool)
     end
 
     # Mandatory periods enforcement
-    @show mandatoryPeriods = get(dft.config["solver"], "mandatoryPeriods", [])
-    replaceVal = type == "hierarchical clustering" ? Float32(0.0) : nothing
-    replaceVal = type == "chronological time period clustering" ? Inf32 : nothing
+    @show mandatoryPeriods = get(dft.config["solver"], "mandatoryPeriods", Int64[])
+    replaceVal = if type == "hierarchical clustering"
+        Float32(0.0)
+    elseif type == "chronological time period clustering"
+        Inf32
+    end
     for i in mandatoryPeriods
         D[i,:] .= replaceVal
     end
     for j in mandatoryPeriods
         D[:,j] .= replaceVal
     end
+
+    # Correct for mandatory periods
     @show NCD = NCD - length(mandatoryPeriods)
-    @show NC = NC - length(mandatoryPeriods)
+    if iszero(NCD)
+        error("Can't run clustering if all periods are fixed.")
+        # TODO: could eventually replace this with a reasonable default.
+        # TODO: currently mandatory periods don't represent other periods!
+        # could be annoying in the future 
+    end
 
-    # TODO: Other possible performance boosts:
-    # 1. Changing the order of the for loops to be j then i ->
-    # be careful with this though.
-    # 2. Using @inbounds -> avoids check in the array bounds when running
-    # 3. Using Threads.@threads (might not speed things up though)
-    # 4. Using Float32 instead of Float64
-    # 5. Can also try out @simd apparently?
+    # Get intermediate periods
+    intermediatePeriods = get(dft.config["solver"], "intermediatePeriods", Int64[])
 
-    intermediatePeriods = get(dft.config["solver"], "intermediatePeriods", [])
-
-    while NC > NCD
+    # Run clustering
+    while NC - length(mandatoryPeriods) > NCD
         if mod(NC, 100) == 0
             @debug "Number of clusters: $NC"
         end
@@ -845,7 +855,6 @@ function timePeriodClustering(dft::DaysFinderTool)
         # This takes up the bulk of the calculation time!
         if type == "chronological time period clustering"
             # Only need to search along a diagonal of D!
-
             D_vec = [
                 @inbounds D[j+1,j] for j in 1:NC-1 # ≈ 0.1 secs
             ]
@@ -855,6 +864,15 @@ function timePeriodClustering(dft::DaysFinderTool)
             idx = CartesianIndex(idx + 1, idx) # ≈ 0.1 secs
         else
             (val, idx) = findmax(D) # ≈ 0.5 secs
+        end
+
+        # Show if it's merging a cluster which is mandatory
+        if IC2P[idx[1]] ∈ mandatoryPeriods || IC2P[idx[2]] ∈ mandatoryPeriods
+            @warn "Something's probably wrong"
+            @show findmax(D)
+            @show NC
+            @show idx[1]
+            @show idx[2]
         end
 
         # Merge the two clusters. Do this by appending the periods of the
@@ -904,43 +922,45 @@ function timePeriodClustering(dft::DaysFinderTool)
 
         # Recompute the dissimilarity for the new cluster w.r.t to the other
         # Clusters
-        # TODO: fix the filtering on the mandatoryPeriods, offset by 1
         if type == "chronological time period clustering"
             i_index = [
-                i for i in max(1, idx[1] - 1):min(NC,idx[1]) if all(IC2P[i] .∉ mandatoryPeriods)
+                i for i in max(1, idx[1] - 1):min(NC,idx[1])
             ]
             j_index = max.(1, i_index .- 1)
         elseif type == "hierarchical clustering"
             i_index = vcat(
                 [idx[1] for i = 1:idx[1]-1],
-                [i for i = idx[1]:NC if all(IC2P[i] .∉ mandatoryPeriods)]
+                [i for i = idx[1]:NC]
             )
             j_index = vcat(
                 [j for j = 1:idx[1]-1],
-                [idx[1] for j = idx[1]:NC if all(IC2P[i] .∉ mandatoryPeriods)]
+                [idx[1] for j = idx[1]:NC]
             )
         end
         @assert length(i_index) == length(j_index)
-        D_idx = [(i_index[k],j_index[k]) for k = 1:length(i_index)]
+        D_idx = [
+            (i_index[k], j_index[k]) for k = 1:length(i_index)
+            if isempty(intersect(IC2P[i_index[k]], mandatoryPeriods))
+            && isempty(intersect(IC2P[j_index[k]], mandatoryPeriods))
+            && i_index[k] != j_index[k]
+        ]
         for (i,j) in D_idx
-            if i != j
-                if i <= 0 || j <= 0
-                    @show i_index
-                    @show j_index
-                    @show NC
-                    return error("Invalid indexing somehow")
-                end
-                num = 2*length(IC2P[i])*length(IC2P[j])
-                den = length(IC2P[i]) + length(IC2P[j])
-                if type == "hierarchical clustering"
-                    @inbounds D[i,j] = Float32(
-                        1 ./ (num/den * sum((xbar[i][:] .- xbar[j][:]) .^ 2))
-                    )
-                else
-                    @inbounds D[i,j] = Float32(
-                        num/den * sum((xbar[i][:] .- xbar[j][:]) .^ 2)
-                    )
-                end
+            # if i <= 0 || j <= 0
+            #     @show i_index
+            #     @show j_index
+            #     @show NC
+            #     return error("Invalid indexing somehow")
+            # end
+            num = 2*length(IC2P[i])*length(IC2P[j])
+            den = length(IC2P[i]) + length(IC2P[j])
+            if type == "hierarchical clustering"
+                @inbounds D[i,j] = Float32(
+                    1 ./ (num/den * sum((xbar[i][:] .- xbar[j][:]) .^ 2))
+                )
+            else
+                @inbounds D[i,j] = Float32(
+                    num/den * sum((xbar[i][:] .- xbar[j][:]) .^ 2)
+                )
             end
         end
 
@@ -955,7 +975,7 @@ function timePeriodClustering(dft::DaysFinderTool)
 
     # Save the results
     calculate_rep_periods_from_clusters!(dft, ICM, IC2P, IP2C)
-    return ICM, IC2P, IP2C
+    return ICM, IC2P, IP2C, D
 end
 
 function calculate_rep_periods_from_clusters!(
