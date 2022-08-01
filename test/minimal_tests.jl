0e2757165e9aa1221bca3c1435467cd63dabecf3
Sebastian Gonzato <sebastian_gonzato@protonmail.com>
2022-08-01 14:35:54 +0200

    Try fixing issue with testing in 1.2

--- test/minimal_tests.jl
+++ test/minimal_tests.jl
@@ -1,10 +1,13 @@
 # Only test essentials, used for testing different julia versions
 using Test
-ENV["GKSwstype"]="nul" #fix gr() problems in headless mode 
+
+try
+    using Pkg
+    Pkg.remove("HiGHS") # To fix issue in 1.2
+catch e
+    @warn e
+end
 
 include(joinpath(@__DIR__, "test_util.jl"))
 include(joinpath(@__DIR__, "test_days_finder.jl"))
 include(joinpath(@__DIR__, "test_saving_and_loading.jl"))
-
-# Remove the files
-rm(joinpath(@__DIR__, "results"), recursive=true)
