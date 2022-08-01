d6e1a92267d5d647a239e0c8244e825699930991
Sebastian Gonzato <sebastian_gonzato@protonmail.com>
2022-08-01 14:59:41 +0200

    Fix HiGHS issue for 1.2

--- test/minimal_tests.jl
+++ test/minimal_tests.jl
@@ -1,13 +1,6 @@
 # Only test essentials, used for testing different julia versions
 using Test
 
-try
-    using Pkg
-    Pkg.remove("HiGHS") # To fix issue in 1.2
-catch e
-    @warn e
-end
-
 include(joinpath(@__DIR__, "test_util.jl"))
 include(joinpath(@__DIR__, "test_days_finder.jl"))
 include(joinpath(@__DIR__, "test_saving_and_loading.jl"))
